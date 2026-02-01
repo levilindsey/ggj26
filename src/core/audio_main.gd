26840f4725191c28bf28c1ac9cab6f09b2a53ffb
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-02-01 15:55:22 -0800

    Fix stuff

--- src/core/audio_main.gd
+++ src/core/audio_main.gd
@@ -10,96 +10,131 @@ extends Node2D
 @export var menu_theme_volume := 0.0
 
 @onready var STREAM_PLAYERS_BY_NAME := {
-    "dino_theme" = %DinoThene,
-    "girl_theme" = %GirlTheme,
-    "pirate_theme" = %PirateTheme,
-    "western_theme" = %WesternTheme,
-    "wizard_theme" = %WizardTheme,
-    "click" = %ClickStreamPlayer,
-    "godot_splash" = %ClickStreamPlayer,
-    "scg_splash" = %SnoringCatStreamPlayer,
-    "success" = %SuccessCadenceStreamPlayer,
-    "failure" = %FailureCadenceStreamPlayer,
-    "achievement" = %AchievementStreamPlayer,
+	"dino_theme" = %DinoTheme,
+	"girl_theme" = %GirlTheme,
+	"pirate_theme" = %PirateTheme,
+	"western_theme" = %WesternTheme,
+	"wizard_theme" = %WizardTheme,
+	"click" = %ClickStreamPlayer,
+	"godot_splash" = %ClickStreamPlayer,
+	"scg_splash" = %SnoringCatStreamPlayer,
+	"success" = %SuccessCadenceStreamPlayer,
+	"failure" = %FailureCadenceStreamPlayer,
+	"achievement" = %AchievementStreamPlayer,
 }
+
+var initial_volumes := {}
+
+var current_theme: AudioStreamPlayer
 
 
 func _enter_tree() -> void:
-    G.audio = self
+	G.audio = self
+
+
+func _ready() -> void:
+	for player_name in STREAM_PLAYERS_BY_NAME:
+		var player: AudioStreamPlayer = STREAM_PLAYERS_BY_NAME[player_name]
+		initial_volumes[player_name] = player.volume_db
 
 
 func play_sound(sound_name: StringName) -> void:
-    if not G.ensure(STREAM_PLAYERS_BY_NAME.has(sound_name)):
-        return
+	if not G.ensure(STREAM_PLAYERS_BY_NAME.has(sound_name)):
+		return
 
-    var stream_player: AudioStreamPlayer = STREAM_PLAYERS_BY_NAME[sound_name]
-    if not stream_player.playing:
-        stream_player.play()
+	var stream_player: AudioStreamPlayer = STREAM_PLAYERS_BY_NAME[sound_name]
+	if not stream_player.playing:
+		stream_player.play()
 
 
-func fade_to_menu_theme() -> void:
-    fade_out(%MainThemeStreamPlayer)
-    fade_in(%MenuThemeStreamPlayer, menu_theme_volume)
+func fade_to_mask_theme(mask_type: Player.MaskType) -> void:
+	var player_name := get_player_name_for_mask(mask_type)
+	fade_to_theme(player_name)
+
+
+func get_player_name_for_mask(mask_type: Player.MaskType) -> String:
+	match mask_type:
+		Player.MaskType.NONE:
+			return "girl_theme"
+		Player.MaskType.COWBOY:
+			return "western_theme"
+		Player.MaskType.PIRATE:
+			return "pirate_theme"
+		Player.MaskType.WIZARD:
+			return "wizard_theme"
+		Player.MaskType.DINOSAUR:
+			return "dino_theme"
+		Player.MaskType.CHICKEN:
+			return "girl_theme"
+		_:
+			G.fatal()
+			return ""
+
+
+func fade_to_theme(theme_name: String) -> void:
+	if is_instance_valid(current_theme):
+		fade_out(current_theme)
+	current_theme = STREAM_PLAYERS_BY_NAME[theme_name]
+	fade_in(current_theme, initial_volumes[theme_name])
 
 
 func fade_to_main_theme() -> void:
-    fade_out(%MenuThemeStreamPlayer)
-    fade_in(%MainThemeStreamPlayer, main_theme_volume)
+	fade_to_mask_theme(Player.MaskType.NONE)
 
 
 func fade_in(stream_player: AudioStreamPlayer, volume: float) -> void:
-    if G.settings.mute_music:
-        volume = mute_volume
+	if G.settings.mute_music:
+		volume = mute_volume
 
-    if not stream_player.playing:
-        stream_player.volume_db = mute_volume
-        # This is similar to calling play(), except play() resets playback
-        # position to zero.
-        stream_player.stream_paused = false
+	if not stream_player.playing:
+		stream_player.volume_db = mute_volume
+		# This is similar to calling play(), except play() resets playback
+		# position to zero.
+		stream_player.stream_paused = false
 
-    var tween := create_tween()
-    tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-    tween.tween_property(
-        stream_player,
-        "volume_db",
-        volume,
-        theme_fade_duration_sec)
+	var tween := create_tween()
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween.tween_property(
+		stream_player,
+		"volume_db",
+		volume,
+		theme_fade_duration_sec)
 
-    await tween.step_finished
-    # Ensure the stream is still playing, just in case we somehow end up with
-    # overlapping tweens (the latest tween should end up winning).
-    stream_player.stream_paused = false
+	await tween.step_finished
+	# Ensure the stream is still playing, just in case we somehow end up with
+	# overlapping tweens (the latest tween should end up winning).
+	stream_player.stream_paused = false
 
 
 func fade_out(stream_player: AudioStreamPlayer) -> void:
-    if not stream_player.playing:
-        return
+	if not stream_player.playing:
+		return
 
-    var tween := create_tween()
-    tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
-    tween.tween_property(
-        stream_player,
-        "volume_db",
-        mute_volume,
-        theme_fade_duration_sec)
+	var tween := create_tween()
+	tween.set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
+	tween.tween_property(
+		stream_player,
+		"volume_db",
+		mute_volume,
+		theme_fade_duration_sec)
 
-    await tween.step_finished
-    # Ensure the stream is still playing, just in case we somehow end up with
-    # overlapping tweens (the latest tween should end up winning).
-    stream_player.stream_paused = true
+	await tween.step_finished
+	# Ensure the stream is still playing, just in case we somehow end up with
+	# overlapping tweens (the latest tween should end up winning).
+	stream_player.stream_paused = true
 
 
 func apply_music_mute() -> void:
-    # Apply mute setting to currently playing music streams.
-    var menu_player := %MenuThemeStreamPlayer
-    var main_player := %MainThemeStreamPlayer
+	# Apply mute setting to currently playing music streams.
+	var menu_player := %MenuThemeStreamPlayer
+	var main_player := %MainThemeStreamPlayer
 
-    if menu_player.playing and not menu_player.stream_paused:
-        menu_player.volume_db = mute_volume if (
-            G.settings.mute_music
-        ) else menu_theme_volume
+	if menu_player.playing and not menu_player.stream_paused:
+		menu_player.volume_db = mute_volume if (
+			G.settings.mute_music
+		) else menu_theme_volume
 
-    if main_player.playing and not main_player.stream_paused:
-        main_player.volume_db = mute_volume if (
-            G.settings.mute_music
-        ) else main_theme_volume
+	if main_player.playing and not main_player.stream_paused:
+		main_player.volume_db = mute_volume if (
+			G.settings.mute_music
+		) else main_theme_volume
--- src/player/player.gd
+++ src/player/player.gd
@@ -34,12 +34,12 @@ extends Character
 
 
 enum MaskType {
-    NONE,
-    COWBOY,
-    PIRATE,
-    WIZARD,
-    DINOSAUR,
-    CHICKEN,
+	NONE,
+	COWBOY,
+	PIRATE,
+	WIZARD,
+	DINOSAUR,
+	CHICKEN,
 }
 
 const _MAX_HEALTH := 100
@@ -57,15 +57,15 @@ const _MASK_SWAP_COOLDOWN_SEC := 1.0
 
 var last_ability_start_time_sec := -INF
 var is_ability_active: bool:
-    get:
-        return (
-            last_ability_start_time_sec + abilty_duration_sec >
-            G.time.get_play_time()
-        )
+	get:
+		return (
+			last_ability_start_time_sec + abilty_duration_sec >
+			G.time.get_play_time()
+		)
 
 var is_strong_defense: bool:
-    get:
-        return defense > 1.5
+	get:
+		return defense > 1.5
 
 var current_masks: Array[MaskType] = [MaskType.NONE]
 var selected_mask_index := 0
@@ -75,57 +75,57 @@ var last_mask_swap_time_sec := -INF
 
 var current_health := _MAX_HEALTH
 var health_progress: float:
-    get:
-        return current_health / float(_MAX_HEALTH)
+	get:
+		return current_health / float(_MAX_HEALTH)
 
 var half_size := Vector2.INF
 
 var last_invincibility_start_time_sec := -INF
 var is_invincible: bool:
-    get:
-        return (
-            last_invincibility_start_time_sec + _INVINCIBILITY_DURATION_SEC >
-            G.time.get_play_time()
-        )
+	get:
+		return (
+			last_invincibility_start_time_sec + _INVINCIBILITY_DURATION_SEC >
+			G.time.get_play_time()
+		)
 
 var is_dead: bool:
-    get:
-        return current_health == 0
+	get:
+		return current_health == 0
 
 
 func _ready() -> void:
-    super._ready()
-    half_size = Geometry.calculate_half_width_height(
-        collision_shape.shape,
-        false)
+	super._ready()
+	half_size = Geometry.calculate_half_width_height(
+		collision_shape.shape,
+		false)
 
 
 func destroy() -> void:
-    queue_free()
+	queue_free()
 
 
 func _trigger_ability() -> void:
-    G.fatal("Abstract Player._trigger_ability is not implemented")
+	G.fatal("Abstract Player._trigger_ability is not implemented")
 
 
 func _process(delta: float) -> void:
-    super._process(delta)
-    _process_invincibility_blink()
+	super._process(delta)
+	_process_invincibility_blink()
 
 
 func _process_invincibility_blink() -> void:
-    if not is_invincible:
-        visible = true
-    else:
-        var elapsed_invincibility_time := (
-            G.time.get_play_time() - last_invincibility_start_time_sec
-        )
-        visible = (
-            floori(
-                elapsed_invincibility_time /
-                (_INVINCIBILITY_BLINK_PERIOD_SEC / 2.0)
-            ) % 2 == 0
-        )
+	if not is_invincible:
+		visible = true
+	else:
+		var elapsed_invincibility_time := (
+			G.time.get_play_time() - last_invincibility_start_time_sec
+		)
+		visible = (
+			floori(
+				elapsed_invincibility_time /
+				(_INVINCIBILITY_BLINK_PERIOD_SEC / 2.0)
+			) % 2 == 0
+		)
 
 
 func _physics_process(delta: float) -> void:
@@ -168,159 +168,159 @@ func _physics_process(delta: float) -> void:
 
 
 func _update_actions() -> void:
-    super._update_actions()
+	super._update_actions()
 
 
 func _process_animation() -> void:
-    if is_ability_active:
-        # Do nothing. "attack" animation is already playing, and triggering it
-        # again would cause looping.
-        pass
-    else:
-        super._process_animation()
+	if is_ability_active:
+		# Do nothing. "attack" animation is already playing, and triggering it
+		# again would cause looping.
+		pass
+	else:
+		super._process_animation()
 
 
 func play_sound(sound_name: String) -> void:
-    match sound_name:
-        "spawn":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "mask":
-            match mask_type:
-                MaskType.NONE:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.COWBOY:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.PIRATE:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.WIZARD:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.DINOSAUR:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.CHICKEN:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                _:
-                    G.fatal()
-        "ability":
-            match mask_type:
-                MaskType.NONE:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.COWBOY:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.PIRATE:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.WIZARD:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.DINOSAUR:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                MaskType.CHICKEN:
-                    # TODO: ALDEN: Make that magic sound stuff happen, baby.
-                    pass
-                _:
-                    G.fatal()
-        "jump":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "land":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "ouch":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "die":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "mask_scroll":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        "mask_pickup":
-            # TODO: ALDEN: Make that magic sound stuff happen, baby.
-            pass
-        _:
-            G.fatal()
+	match sound_name:
+		"spawn":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"mask":
+			match mask_type:
+				MaskType.NONE:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.COWBOY:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.PIRATE:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.WIZARD:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.DINOSAUR:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.CHICKEN:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				_:
+					G.fatal()
+		"ability":
+			match mask_type:
+				MaskType.NONE:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.COWBOY:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.PIRATE:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.WIZARD:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.DINOSAUR:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				MaskType.CHICKEN:
+					# TODO: ALDEN: Make that magic sound stuff happen, baby.
+					pass
+				_:
+					G.fatal()
+		"jump":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"land":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"ouch":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"die":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"mask_scroll":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		"mask_pickup":
+			# TODO: ALDEN: Make that magic sound stuff happen, baby.
+			pass
+		_:
+			G.fatal()
 
 
 func pick_up_mask(p_mask_type: MaskType) -> void:
-    if current_masks.has(p_mask_type):
-        return
-    current_masks.append(p_mask_type)
-    G.level.swap_mask(p_mask_type)
-    play_sound("mask_pickup")
+	if current_masks.has(p_mask_type):
+		return
+	current_masks.append(p_mask_type)
+	G.level.swap_mask(p_mask_type)
+	play_sound("mask_pickup")
 
 
 func take_damage(damage: int) -> void:
-    if is_dead:
-        # Ignore damage. Already dead.
-        return
-    if is_invincible:
-        # Ignore damage. Still invincible.
-        return
+	if is_dead:
+		# Ignore damage. Already dead.
+		return
+	if is_invincible:
+		# Ignore damage. Still invincible.
+		return
 
-    var current_time := G.time.get_play_time()
+	var current_time := G.time.get_play_time()
 
-    var previous_health := current_health
-    var modified_damage := floori(damage / defense)
-    current_health = maxi(current_health - modified_damage, 0)
+	var previous_health := current_health
+	var modified_damage := floori(damage / defense)
+	current_health = maxi(current_health - modified_damage, 0)
 
-    if current_health == 0:
-        die()
-    else:
-        G.print(
-            "Player damaged: %s => %s" % [previous_health, current_health],
-            ScaffolderLog.CATEGORY_GAME_STATE)
-        last_invincibility_start_time_sec = current_time
-        play_sound("ouch")
+	if current_health == 0:
+		die()
+	else:
+		G.print(
+			"Player damaged: %s => %s" % [previous_health, current_health],
+			ScaffolderLog.CATEGORY_GAME_STATE)
+		last_invincibility_start_time_sec = current_time
+		play_sound("ouch")
 
-    G.hud.update_health()
+	G.hud.update_health()
 
 
 func die() -> void:
-    G.print("Player died", ScaffolderLog.CATEGORY_GAME_STATE)
-    play_sound("die")
-    G.level.game_over()
+	G.print("Player died", ScaffolderLog.CATEGORY_GAME_STATE)
+	play_sound("die")
+	G.level.game_over()
 
 
 func copy(other: Player) -> void:
-    global_position = other.global_position
-    velocity = other.velocity
-    current_health = other.current_health
-    current_masks = other.current_masks
-    previous_mask_type = other.mask_type
+	global_position = other.global_position
+	velocity = other.velocity
+	current_health = other.current_health
+	current_masks = other.current_masks
+	previous_mask_type = other.mask_type
 
-    if mask_type == MaskType.NONE:
-        # When toggling off a mask, to revert back to the girl, remain with the
-        # previous mask selected.
-        selected_mask_index = other.selected_mask_index
-    else:
-        selected_mask_index = current_masks.find(mask_type)
-    G.check(selected_mask_index >= 0)
+	if mask_type == MaskType.NONE:
+		# When toggling off a mask, to revert back to the girl, remain with the
+		# previous mask selected.
+		selected_mask_index = other.selected_mask_index
+	else:
+		selected_mask_index = current_masks.find(mask_type)
+	G.check(selected_mask_index >= 0)
 
 
 static func get_palette_swap_index_for_mask(p_mask_type: MaskType) -> int:
-    match p_mask_type:
-        MaskType.NONE:
-            return 1
-        MaskType.COWBOY:
-            return 3
-        MaskType.PIRATE:
-            return 4
-        MaskType.WIZARD:
-            return 5
-        MaskType.DINOSAUR:
-            return 2
-        MaskType.CHICKEN:
-            return 1
-        _:
-            G.fatal()
-            return 0
+	match p_mask_type:
+		MaskType.NONE:
+			return 1
+		MaskType.COWBOY:
+			return 3
+		MaskType.PIRATE:
+			return 4
+		MaskType.WIZARD:
+			return 5
+		MaskType.DINOSAUR:
+			return 2
+		MaskType.CHICKEN:
+			return 1
+		_:
+			G.fatal()
+			return 0
