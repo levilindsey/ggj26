beb7c9445eb15c3af68da24ce0aeaf5cd44d34c5
Levi Lindsey <4317587+levilindsey@users.noreply.github.com>
2026-02-01 16:54:02 -0800

    Enemy config fixes

--- src/scaffolder/character/character_animator.gd
+++ src/scaffolder/character/character_animator.gd
@@ -7,15 +7,29 @@ extends Node2D
 
 var _is_facing_right := true
 
+var initial_animated_sprite_position := Vector2.INF
+
+
+func _ready() -> void:
+	initial_animated_sprite_position = animated_sprite.position
+
 
 func face_left() -> void:
 	_is_facing_right = false
 	animated_sprite.flip_h = faces_right_by_default
+	if animated_sprite.flip_h:
+		animated_sprite.position.x = -initial_animated_sprite_position.x
+	else:
+		animated_sprite.position.x = initial_animated_sprite_position.x
 
 
 func face_right() -> void:
 	_is_facing_right = true
 	animated_sprite.flip_h = not faces_right_by_default
+	if animated_sprite.flip_h:
+		animated_sprite.position.x = -initial_animated_sprite_position.x
+	else:
+		animated_sprite.position.x = initial_animated_sprite_position.x
 
 
 func play(animation_name: String) -> void:
